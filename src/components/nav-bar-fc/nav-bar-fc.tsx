ac02bd7afc76a933eca4a63f1649567621c7fcd5
daversd <dave_rsd@hotmail.com>
2023-04-30 16:23:14 -0300

    nav bar responsiva com menu

--- src/components/nav-bar-fc/nav-bar-fc.tsx
+++ src/components/nav-bar-fc/nav-bar-fc.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import style from '../../style-sheets/main-style.module.scss';
@@ -9,11 +9,37 @@ export const NavBarFC: FC = () => {
     <div className={style.navContainer}>
       <div className={style.navTitleContainer}><div className={style.navTitle}>AAC</div></div>
       <SearchBarFC />
-      <div className={style.navLi}>
-          <Link className={style.navLinkText} to='/'>pesquise</Link>
-          <Link className={style.navLinkText} to='/sobre'>sobre</Link>
-          <Link className={style.navLinkText} to='/colabore'>colabore</Link>
-      </div>
+      <NavBarLinks />
+      <NavBarMenu />
     </div>
   </nav>
 };
+
+const NavBarLinks: FC = () => {
+  return <div className={style.navLi}>
+    <Link className={style.navLinkText} to='/'>pesquise</Link>
+    <Link className={style.navLinkText} to='/sobre'>sobre</Link>
+    <Link className={style.navLinkText} to='/colabore'>colabore</Link>
+  </div>
+}
+
+const NavBarMenu: FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  return <>
+    <div className={style.navMenuButtonContainer}>
+      <div className={style.navMenuButton} onClick={() => setIsOpen(!isOpen)}>
+        <div className={style.navMenuButtonLine}></div>
+        <div className={style.navMenuButtonLine}></div>
+        <div className={style.navMenuButtonLine}></div>
+      </div>
+    </div>
+    {isOpen && <div>
+      <div className={style.navMenuBlocker} onClick={() => setIsOpen(!isOpen)}></div>
+      <div className={style.navMenuContainer}>
+        <Link className={style.navMenuLink} to='/'>pesquise</Link>
+        <Link className={style.navMenuLink} to='/sobre'>sobre</Link>
+        <Link className={style.navMenuLink} to='/colabore'>colabore</Link>
+      </div>
+    </div>}
+  </>
+}
