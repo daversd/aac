3f7f025ea5a57005aa671d471ba4bc5507a714a2
daversd <dave_rsd@hotmail.com>
2023-05-01 16:31:15 -0300

    tags funcionais

--- src/components/entry-fc/entry-fc.tsx
+++ src/components/entry-fc/entry-fc.tsx
@@ -1,6 +1,7 @@
-import { FC, useState } from 'react';
+import { FC, useCallback, useState } from 'react';
 import { Entry } from '../../core/entry';
 import mainStyle from '../../style-sheets/main-style.module.scss';
+import { useSearchParams } from 'react-router-dom';
 
 export interface EntryFCProps {
   entry: Entry;
@@ -8,17 +9,34 @@ export interface EntryFCProps {
 
 export const EntryFC: FC<EntryFCProps> = ({ entry }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [_, setSearchParams] = useSearchParams();
+
+  const setYearFilter = useCallback((value: string) => {
+    setSearchParams({years: value});
+  }, []);
+
+  const setAuthorFilter = useCallback((value: string) => {
+    setSearchParams({authors: value});
+  }, []);
+  
+  const setKeywordsFilter = useCallback((value: string) => {
+    setSearchParams({keywords: value});
+  }, []);
+  
+  const setTypesFilter = useCallback((value: string) => {
+    setSearchParams({types: value});
+  }, []);
   
   return <div className={mainStyle.entryContainer}>
     <label className={mainStyle.entryTitle}>{entry.name}</label>
     <div className={mainStyle.entryExpandButton} onClick={() => setIsOpen(!isOpen)}>{`${isOpen ? '-' : '+'}`}</div>
-    <EntryProperty value={[entry.year.toString()]} label={'Ano'} />
+    <EntryProperty value={[entry.year.toString()]} label={'Ano'} onClick={setYearFilter}/>
     <div className={mainStyle.regularContainer}>
       <p className={isOpen ? mainStyle.p : mainStyle.pCollapsed}>{entry.abstract}</p>
     </div>
-    <EntryProperty value={entry.authors} label={'Autorxs'} />
-    {isOpen && <EntryProperty value={entry.keywords} label={'Palavras-chave'} />}
-    {isOpen && <EntryProperty value={entry.types} label={'Tipos'} />}
+    <EntryProperty value={entry.authors} label={'Autorxs'} onClick={setAuthorFilter}/>
+    {isOpen && <EntryProperty value={entry.keywords} label={'Palavras-chave'} onClick={setKeywordsFilter}/>}
+    {isOpen && <EntryProperty value={entry.types} label={'Tipos'} onClick={setTypesFilter}/>}
     {isOpen &&  <div className={mainStyle.regularContainer}>
       <label className={mainStyle.entryPropLabel}>Links: </label>
       {entry.links.map((l, i) =>
@@ -34,13 +52,14 @@ export const EntryFC: FC<EntryFCProps> = ({ entry }) => {
 interface EntryPropertyProps {
   value: string[];
   label: string;
+  onClick?: (val: string) => void;
 }
 
-const EntryProperty: FC<EntryPropertyProps> = ({ value, label }) => {
+const EntryProperty: FC<EntryPropertyProps> = ({ value, label, onClick }) => {
   return <div className={mainStyle.regularContainer}>
     <label className={mainStyle.entryPropLabel}>{label}:</label>
     {value.map((v, i) =>
-      <EntryTag key={i} value={v} />
+      <EntryTag key={i} value={v} onClick={onClick}/>
     )}
 
   </div>
@@ -52,5 +71,5 @@ interface EntryTagProps {
 }
 
 const EntryTag: FC<EntryTagProps> = ({ value, onClick }) => {
-  return <div className={mainStyle.entryTag} >{value}</div>
+  return <div className={mainStyle.entryTag} onClick={onClick && (() => onClick(value))}>{value}</div>
 }
