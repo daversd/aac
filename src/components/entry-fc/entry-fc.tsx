6e56db4e6081ac352d1a10605a9678f44cacaadf
daversd <dave_rsd@hotmail.com>
2023-05-01 16:19:54 -0300

    entry styling

--- src/components/entry-fc/entry-fc.tsx
+++ src/components/entry-fc/entry-fc.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Entry } from '../../core/entry';
 import mainStyle from '../../style-sheets/main-style.module.scss';
 
@@ -7,35 +7,50 @@ export interface EntryFCProps {
 }
 
 export const EntryFC: FC<EntryFCProps> = ({ entry }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  
   return <div className={mainStyle.entryContainer}>
     <label className={mainStyle.entryTitle}>{entry.name}</label>
+    <div className={mainStyle.entryExpandButton} onClick={() => setIsOpen(!isOpen)}>{`${isOpen ? '-' : '+'}`}</div>
+    <EntryProperty value={[entry.year.toString()]} label={'Ano'} />
     <div className={mainStyle.regularContainer}>
-      <label className={mainStyle.label}>Ano: </label>
-      <label className={mainStyle.regular}>{entry.year}</label>
+      <p className={isOpen ? mainStyle.p : mainStyle.pCollapsed}>{entry.abstract}</p>
     </div>
-    <div className={mainStyle.regularContainer}>
-      <p className={mainStyle.p}>{entry.abstract}</p>
-    </div>
-    <div className={mainStyle.regularContainer}>
-      <label className={mainStyle.label}>Autores: </label>
-      <label className={mainStyle.regular}>{entry.authors.join(' | ')}</label>
-    </div>
-    <div className={mainStyle.regularContainer}>
-      <label className={mainStyle.label}>Palavras-chave: </label>
-      <label className={mainStyle.regular}>{entry.keywords.join(' | ')}</label>
-    </div>
-    <div className={mainStyle.regularContainer}>
-      <label className={mainStyle.label}>Tipos: </label>
-      <label className={mainStyle.regular}>{entry.types.join(' | ')}</label>
-    </div>
-    <div className={mainStyle.regularContainer}>
-    <label className={mainStyle.label}>Links: </label>
+    <EntryProperty value={entry.authors} label={'Autorxs'} />
+    {isOpen && <EntryProperty value={entry.keywords} label={'Palavras-chave'} />}
+    {isOpen && <EntryProperty value={entry.types} label={'Tipos'} />}
+    {isOpen &&  <div className={mainStyle.regularContainer}>
+      <label className={mainStyle.entryPropLabel}>Links: </label>
       {entry.links.map((l, i) =>
         <div key={i}>
           <a className={mainStyle.regularLink} href={l}> {l} </a>
           <br />
         </div>
       )}
-    </div>
+    </div>}
   </div>;
 };
+
+interface EntryPropertyProps {
+  value: string[];
+  label: string;
+}
+
+const EntryProperty: FC<EntryPropertyProps> = ({ value, label }) => {
+  return <div className={mainStyle.regularContainer}>
+    <label className={mainStyle.entryPropLabel}>{label}:</label>
+    {value.map((v, i) =>
+      <EntryTag key={i} value={v} />
+    )}
+
+  </div>
+}
+
+interface EntryTagProps {
+  value: string;
+  onClick?: (val: string) => void;
+}
+
+const EntryTag: FC<EntryTagProps> = ({ value, onClick }) => {
+  return <div className={mainStyle.entryTag} >{value}</div>
+}
